2b9ad776ca6446e3d54717ac10b4564b090b261a
snehaMishra27 <snehamis380@gmail.com>
2026-03-27 23:26:33 +0530

    Initial commit - AlphaBurn

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,39 +1,3 @@
-
-// const API_BASE = "http://localhost:5000";
-
-// export const apiFetch = async (url, options = {}) => {
-//   const response = await fetch(`${API_BASE}${url}`, {
-//     credentials: "include", // session cookie
-//     headers: {
-//       "Content-Type": "application/json",
-//       ...(options.headers || {}),
-//     },
-//     ...options,
-//   });
-
-//   // read response ONCE
-//   const text = await response.text();
-
-//   let data;
-//   try {
-//     data = text ? JSON.parse(text) : {};
-//   } catch {
-//     throw new Error("Server did not return JSON");
-//   }
-
-//   // handle auth failure cleanly
-//   if (response.status === 401) {
-//     throw new Error("Unauthorized");
-//   }
-
-//   if (!response.ok) {
-//     throw new Error(data.error || data.message || "Something went wrong");
-//   }
-
-//   return data;
-// };
-
-
 import axios from "axios";
 
 const api = axios.create({
