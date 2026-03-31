a3450721aad6aad9637c521c7af161b783794f3c
snehaMishra27 <snehamis380@gmail.com>
2026-03-31 14:47:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const cors = require("cors");
