fc01c3d931f83756b9c11d46a7309dfbbb58dd1b
snehaMishra27 <snehamis380@gmail.com>
2026-03-31 14:53:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -54,7 +54,7 @@ mongoose
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/{*path}", (req, res) => {
     res.sendFile(
       path.join(__dirname, "frontend/build/index.html")
     );
